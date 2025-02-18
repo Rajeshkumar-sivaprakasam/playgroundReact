c5a1070adf428839f06422400b8e8dba6b2eb64e
rajesh <rk.sivaprakasam@outlook.com>
2025-02-18 18:42:38 +0530

    feat(tailwind configured): tailwind is added in vite config way && docs noted

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,8 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>helo</h1>
-      Home
+      <h1 className="text-3xl font-bold underline">helo</h1>
     </div>
   );
 };
